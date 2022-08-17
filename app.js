48e15164b7ab9c3e18c9a546de076bf4bf1b7a3f
rafay <rafayf3@gmail.com>
2022-08-17 22:13:58 +0545

    Initial commit

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-let a, date, time, seconds;
+let a, date, time, seconds,minutes,session;
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 setInterval(() => {
+    session = 'AM'
     a = new Date();
+    if(a.getHours() > 12){
+        session = 'PM'
+    }
     date = a.toLocaleDateString(undefined, options);
     seconds = String(a.getSeconds()).padStart(2, '0');
-    time = a.getHours() + ':' + a.getMinutes() + ':' + seconds;
+    minutes = String(a.getMinutes()).padStart(2, '0');
+    time = a.getHours() + ':' + minutes + ':' + seconds + " " + session;
     document.getElementById('time').innerHTML = time + "<br>on " + date;
 }, 1000);
